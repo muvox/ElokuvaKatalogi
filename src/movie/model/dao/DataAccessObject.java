94efaf1e8649be58ec37d7ff363d4da7e04d2e45
Joni Niemi <muvoksi@gmail.com>
2019-03-27 14:06:51 +0200

    Update DataAccessObject.java

--- src/movie/model/dao/DataAccessObject.java
+++ src/movie/model/dao/DataAccessObject.java
@@ -19,7 +19,7 @@ public class DataAccessObject {
 		String username = "a1704471";  
 		// dbk�ytt�j�tunnuksesi on t�ss� a-alkuinen opiskelijanumerosi 
 		
-		String password = "tiDI1P83z";
+		String password = "XXXX";
 		String url = "jdbc:mysql://localhost:3306/a1704471";
 		// tietokantasi nimi on t�ss� a-alkuinen opiskelijanumerosi
 
